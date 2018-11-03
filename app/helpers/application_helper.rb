f82fe5266081e5b6818f7886491062ef1833a5be
Tho Nguyen <nghtho94@gmail.com>
2018-11-03 22:51:07 +0700

    Add full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Return Full page title on a page
+  def full_title(page_title = '')
+    base_title = "STRUTIN' Chuyên Thời Trang Nam"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
